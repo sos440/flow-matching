5be02e150236955f33604b4f819bb00bcdad8f39
Sangchul Lee <sosintegral87@gmail.com>
2024-10-08 14:02:14 +0900

    added direct matching simulations

---
+++ dm_model_simple.py
@@ -0,0 +1,128 @@
+# Torch
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+# ODE Solver
+from zuko.utils import odeint
+
+
+# Choose device
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+elif torch.backends.mps.is_available():
+    device = torch.device("mps")
+else:
+    device = torch.device("cpu")
+
+
+####################################################################################################
+
+
+class VectorField(nn.Module):
+    def __init__(self, net: nn.Module) -> None:
+        """
+        Construct a vector field from a neural network.
+        This is essentially a wrapper around the neural network that allows to solve ODEs.
+
+        Args:
+            `net` (`nn.Module`): The neural network representing the vector field. It must take two inputs:
+
+                - `t` (`Tensor`): The time. It is either a scalar or a tensor of shape `(batch_size)`.
+                - `x` (`Tensor`): The state. It is a tensor of shape `(batch_size, state_dim)`.
+
+            and output the vector field at the given time and state. It must return a tensor of shape `(batch_size, state_dim)`.
+
+        Returns:
+            None
+        """
+        super().__init__()
+        self.net = net
+
+    def forward(self, t: float | Tensor, x: Tensor) -> Tensor:
+        if not isinstance(t, Tensor):
+            t = torch.tensor(t, device=x.device)
+        if t.dim() == 0:
+            t = t.expand(x.shape[0])
+
+        return self.net(t, x)
+
+    def odeint(self, x_0: Tensor, t_0: float, t_1: float) -> Tensor:
+        """
+        Solve the ODE given the initial state `x0` at time `t0` and the final time `t1`.
+        """
+        return odeint(self.forward, x_0, t_0, t_1, self.parameters())
+
+    def encode(self, x_data: Tensor) -> Tensor:
+        """
+        Return the noised samples given the data samples.
+        """
+        return self.odeint(x_data, 0.0, 1.0)
+
+    def decode(self, x_noise: Tensor):
+        """
+        Return the data samples given the noised samples.
+        """
+        return self.odeint(x_noise, 1.0, 0.0)
+
+
+####################################################################################################
+
+
+def dm_loss(v: VectorField, x_data: Tensor, x_noise: Tensor, T: float = 100) -> Tensor:
+    """
+    Compute the direct-matching loss.
+    """
+
+    assert x_data.shape == x_noise.shape, "The data and noise tensors must have the same shape."
+    assert x_data.device == x_noise.device, "The data and noise tensors must be on the same device."
+
+    # Time
+    t = torch.rand(x_data.shape[0], device=x_data.device)[:, None]
+
+    # Integration variable
+    s = T * torch.rand(x_data.shape[0], device=x_data.device)[:, None]
+
+    # Noise
+    z = torch.randn(x_data.shape, device=x_data.device)
+
+    loss_batch = (1 - t) * torch.sum(v(t, x_data) ** 2, dim=-1) + t * torch.sum(v(t, x_noise) ** 2, dim=-1)
+    loss_batch = loss_batch + 2 * torch.sum(T * (v(t, x_noise + s * z) - v(t, x_data + s * z)) * z, dim=-1)
+
+    return torch.mean(loss_batch)
+
+
+####################################################################################################
+
+
+class Net(nn.Module):
+    def __init__(self, in_dim: int, out_dim: int, h_dims: list[int], n_frequencies: int) -> None:
+        super().__init__()
+
+        ins = [in_dim + 2 * n_frequencies] + h_dims
+        outs = h_dims + [out_dim]
+        self.n_frequencies = n_frequencies
+
+        self.layers = nn.ModuleList(
+            [nn.Sequential(nn.Linear(in_d, out_d), nn.LeakyReLU()) for in_d, out_d in zip(ins, outs)]
+        )
+        self.top = nn.Sequential(nn.Linear(out_dim, out_dim))
+
+    def time_encoder(self, t: Tensor) -> Tensor:
+        if t.dim() == 1:
+            t = t[..., None]
+        freq = 2 * torch.arange(self.n_frequencies, device=t.device) * torch.pi
+        t = freq * t
+        return torch.cat((t.cos(), t.sin()), dim=-1)
+
+    def forward(self, t: Tensor, x: Tensor) -> Tensor:
+        # Extend the input with the time encoding
+        t = self.time_encoder(t)
+        x = torch.cat((x, t), dim=-1)
+
+        # Pass through the layers
+        for l in self.layers:
+            x = l(x)
+        x = self.top(x)
+
+        return x
---
+++ dm_test_1d.py
@@ -0,0 +1,114 @@
+from dm_model_simple import *
+from torch.utils.data import TensorDataset, DataLoader
+
+# Other tools
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from numpy.typing import NDArray
+
+
+################################################################################
+
+print(f"Device selected: {device}")
+
+
+################################################################################
+
+# Generate the dataset
+n_points = 10_000
+x_data_0 = 2 * torch.rand(n_points, 1, device=device) - 1
+
+# Prepare the dataloader
+batch_size = 2048
+dataset = TensorDataset(x_data_0)
+dataloader = DataLoader(dataset, batch_size=batch_size)
+
+# Train
+net = Net(1, 1, [512] * 5, 10).to(device)
+v_t = VectorField(net)
+
+# configure optimizer
+optimizer = torch.optim.Adam(v_t.parameters(), lr=1e-3)
+n_epochs = 1000
+
+losses = np.zeros((n_epochs))
+for epoch in tqdm(range(n_epochs), ncols=88):
+    for batch in dataloader:
+        x_data = batch[0]
+        x_noise = torch.randn_like(x_data)
+
+        # Epoch step
+        optimizer.zero_grad()
+        loss = dm_loss(v_t, x_data, x_noise)
+        loss.backward()
+        optimizer.step()
+
+        # Record loss
+        losses[epoch] = loss.item()
+
+# Display the learning curve
+plt.plot(np.arange(len(losses)), losses)
+plt.xlabel("Epoch")
+plt.ylabel("Flow Matching Loss")
+plt.savefig("outputs/1d_learning_curve.png", dpi=300)
+plt.close()
+
+################################################################################
+
+# Generate samples using the learned vector field
+n_samples = 10_000
+with torch.no_grad():
+    x_noise = torch.randn(n_samples, 1, device=device)
+    x_data_pred = v_t.decode(x_noise)
+
+x_data_0 = x_data_0.cpu().numpy()
+x_data_pred = x_data_pred.cpu().numpy()
+
+# Display the generated data
+plt.hist(x_data_0, bins=128, alpha=0.5, label="Data")
+plt.hist(x_data_pred, bins=128, alpha=0.5, label="Generated")
+plt.tight_layout()
+plt.legend()
+plt.savefig("outputs/1d_sample_comparison.png", dpi=300)
+plt.close()
+
+# ################################################################################
+
+# from matplotlib.collections import PathCollection
+# from matplotlib.path import Path
+
+
+# def to_path_code(path):
+#     assert len(path) >= 2
+#     codes = [Path.MOVETO] + [Path.LINETO] * (len(path) - 1)
+#     return codes
+
+
+# # Sampling
+# n_samples = 500
+# n_steps = 200
+# t_steps = torch.linspace(0, 1, n_steps, device=device)
+
+# x_t = []
+# with torch.no_grad():
+#     x_t.append(pd.p_0((n_samples, 1)).to(device=device))
+#     for i in range(1, n_steps):
+#         x_t.append(v_t.decode_t0_t1(x_t[-1], t_steps[i - 1], t_steps[i]))
+
+# # Processing
+# x_t_numpy = np.zeros((n_steps, n_samples, 2))
+# for i in range(n_steps):
+#     x_t_numpy[i] = np.hstack([np.full((n_samples, 1), t_steps[i].item()), x_t[i].detach().cpu().numpy()])
+
+# paths = x_t_numpy.transpose(1, 0, 2)
+
+# # Drawing
+# fig, ax = plt.subplots(figsize=(10, 6))
+
+# for path in paths:
+#     ax.plot(path[:, 0], path[:, 1], color="blue", alpha=0.1)
+
+# plt.savefig("outputs/1d_sample_gen_trajectory.png", dpi=300)
+# plt.close()
